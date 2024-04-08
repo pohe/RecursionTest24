dddb2031032a626be19efed7e665bccf73bc0fb4
Poul Henriksen <POHE@zealand.dk>
2024-04-08 10:01:40 +0200

    Add project files.

---
+++ RecursionTest24/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecursionTest24;
+
+Console.WriteLine("RecursionTest24!");
+
+
+RecursionEx r = new RecursionEx();
+
+r.PrintHello(3);
+Console.WriteLine("Print hello FOR");
+r.PrintHelloItFor(3);
+
+Console.WriteLine("Print hello While");
+r.PrintHelloItWhile(3);
+
+Console.WriteLine("Adnumbersiterativt");
+Console.WriteLine(r.AddNumbersIterativt(3));
+
+Console.WriteLine("AddNumbersRecursive");
+Console.WriteLine(r.AddNumbersRecursive(3));
---
+++ RecursionTest24/RecursionEx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionTest24
+{
+    public class RecursionEx
+    {
+
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello");
+            PrintHello();
+        }
+
+        public void PrintHello(int antalKald)
+        {
+            if (antalKald > 0)
+            {
+                Console.WriteLine("Hello");
+                PrintHello(antalKald - 1);
+            }
+
+        }
+
+        public void PrintHelloItWhile(int antalKald)
+        {
+            while (antalKald > 0)
+            {
+                Console.WriteLine("Hello");
+                antalKald--;
+            }
+        }
+
+        public void PrintHelloItFor(int antalKald)
+        {
+            for (int i = 0; i < antalKald; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        //Finder summen fra 0 og op til og med number
+        public int AddNumbersIterativt(int number)
+        {
+            int total = 0;
+            //for(int i = 1;i <= number;i++)
+            //{
+            //    total += i; //total = total +i;
+            //}
+            int currentNumber = 1;
+            while (number >= currentNumber)
+            {
+                total += currentNumber;
+                currentNumber++;
+            }
+            return total;
+        }
+
+        public int AddNumbersRecursive(int number)
+        {
+            if (number>0)
+            {
+                return number + AddNumbersRecursive(number - 1);
+            }
+            return 0;
+        }
+    } 
+}
